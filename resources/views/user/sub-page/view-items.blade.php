256ea5cd4e08d1bde41514ff9cf05c248a232d74
Bnzr <ebenezer.corpin.spcpc@gmail.com>
2023-05-19 00:21:51 +0800

    receiver name, not done

--- resources/views/user/sub-page/view-items.blade.php
+++ resources/views/user/sub-page/view-items.blade.php
@@ -9,13 +9,7 @@
         <div class="col-md-9 col-lg-10 p-0">
             <div id="content" class="px-2 py-1">
                 <div class="container-lg">
-                    @if($request->status !== 'completed')
-                    <a href="{{route('user.viewRequest',['request' => 'pending'])}}" class="btn btn-secondary">Back</a>
-                    @endif
-
-                    @if($request->status === 'completed')
-                    <a href="{{route('user.viewRequest',['request' => 'completed'])}}" class="btn btn-secondary">Back</a>
-                    @endif
+                    <a href="{{ route('user.viewRequest',['request' => $request->status, 'filter' => 'today']) }}" class="btn btn-secondary">Back</a>
                 </div>
                 <div class="container-md mt-3 pt-2 pb-2 shadow lh-1 rounded overflow-auto">
                     <h4>Request Details</h4>
@@ -24,7 +18,7 @@
                             <tr>
                                 <th scope="col" style="border: white 1px solid;">Request ID</th>
                                 <th scope="col" style="border: white 1px solid;">Office</th>
-                                <th scope="col" style="border: white 1px solid;">Request By</th>
+                                <th scope="col" style="border: white 1px solid;">Requester</th>
                                 <th scope="col" style="border: white 1px solid;">Request To</th>
                                 <th scope="col" style="border: white 1px solid;">Request status</th>
                                 @if(!is_null($request->accepted_by_user_name))
@@ -67,9 +61,9 @@
                         </tbody>
                     </table>
                 </div>
-                <div class="container-lg p-0 border border-dark shadow p-3 mt-3 mb-2 bg-body rounded" style="height: 340px;">
+                <div class="container-lg p-0 border border-dark shadow p-3 mt-3 mb-2 bg-body rounded" style="height: auto">
                     <h4>Requested Items</h4>
-                    <div class=" container-lg p-0" style="height:230px;overflow-y: auto;">
+                    <div class=" container-lg p-0 mb-2 border" style="height:230px;overflow-y: auto;">
                         <table class="table">
                             <thead class="text-white bg-secondary" style="position: sticky;top: 0;z-index: 0;">
                                 <tr>
@@ -108,9 +102,31 @@
                         </table>
                     </div>
                     @if($request->status == 'delivered')
+
                     <form action="{{ route('user.receive-request',['rid' => $request->id]) }}" method="post">
                         @csrf
-                        <button type="submit" class="btn btn-warning shadow">Received</button>
+                        <div class="container-md d-flex m-0 p-0" style="flex-direction:column;">
+                            <div class="container-md p-0">
+                                <label for="receiver_name" class="px-1"><strong>RECEIVER NAME</strong></label>
+                            </div>
+
+                            <div class="container-md p-0 mt-2">
+                                <div class="container-md">
+                                    <label for="requester">Requester</label>
+                                    <input type="radio" name="receiver_name" id="requester" value="{{ $request->request_by }}" onchange="checkOtherOption()">
+                                </div>
+
+                                <div class="container-md">
+                                    <label for="other">Other</label>
+                                    <input type="radio" name="receiver_name" id="other" onchange="checkOtherOption()">
+                                    <input type="text" class="form-control" name="receiver_name" id="receiver_name" style="max-width: 400px; display:none;">
+                                </div>
+
+                                <div class="container-md mt-1">
+                                    <button type="submit" class="btn btn-warning shadow">Received</button>
+                                </div>
+                            </div>
+                        </div>
                     </form>
                     @endif
                 </div>
@@ -118,4 +134,21 @@
         </div>
     </div>
 </div>
+<script>
+    function checkOtherOption() {
+        const requesterRadio = document.getElementById("requester");
+        const otherRadio = document.getElementById("other");
+        const receiverNameInput = document.getElementById("receiver_name");
+
+        if (otherRadio.checked) {
+            const receiverName = receiverNameInput.value;
+            receiverNameInput.style.display = "block";
+            console.log(receiverName);
+        } else if (requesterRadio.checked) {
+            receiverNameInput.value = "";
+            receiverNameInput.style.display = "none";
+            console.log(requesterRadio.value);
+        }
+    }
+</script>
 @endsection
